eae8d348c5c5b09ba19e28eec424e841d7a1ac47
iriszhang <zhang.tianyi13@northeastern.edu>
2024-10-01 21:17:19 -0700

    add 3 failed tests - source untouched

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,24 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
+    def test_invalid_quality(self):  # quality is > 50
+        items = [Item("apple", 0, 52)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(52, items[0].quality)
+
+    def test_aged_brie_quality(self):  # quality
+        items = [Item("Aged Brie", 1, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(1, items[0].quality) # should be 2
+
+    def test_sulfuras_quality(self):  # Sulfuras never has to be sold or decreases in Quality
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 15)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(16, items[0].quality)  # should be 15
+
 
 if __name__ == '__main__':
     unittest.main()
