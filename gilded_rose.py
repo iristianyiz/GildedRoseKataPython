f8897a01e9b7acc777b4ff57b3c7889c6ca3cb84
iriszhang <zhang.tianyi13@northeastern.edu>
2024-10-08 21:20:00 -0700

    lab 5 - Gilded Rose (strategy pattern &tests pass)

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
 
 class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
@@ -12,38 +14,75 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
+class ItemStrategy(ABC):
+    """Abstract base class for all Item strategies."""
+
+    @abstractmethod
+    def update_quality(self, item: Item):
+        pass
+
+
+class NormalItemStrategy(ItemStrategy):
+    """Strategy for normal items that degrade in quality over time."""
+
+    def update_quality(self, item: Item):
+        if item.quality > 0:
+            item.quality -= 1
+        item.sell_in -= 1
+        if item.sell_in < 0 < item.quality: # item.sell_in < 0 and item.quality > 0
+            item.quality -= 1
+
+
+class AgedBrieStrategy(ItemStrategy):
+    """Strategy for Aged Brie, which increases in quality as it gets older."""
+
+    def update_quality(self, item: Item):
+        if item.quality < 50:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+
+class BackstagePassStrategy(ItemStrategy):
+    """Strategy for Backstage passes, which increase in quality as the concert date approaches."""
+
+    def update_quality(self, item: Item):
+        if item.quality < 50:
+            item.quality += 1
+        if item.sell_in < 11 and item.quality < 50:
+            item.quality += 1
+        if item.sell_in < 6 and item.quality < 50:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = 0
+
+
+class SulfurasStrategy(ItemStrategy):
+    """Strategy for Sulfuras, which never changes in quality or sell-in."""
+
+    def update_quality(self, item: Item):
+        pass  # Sulfuras doesn't need any updates
+
+
 class GildedRose(object):
 
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
+    def _get_item_strategy(self, item: Item) -> ItemStrategy:
+        if item.name == "Aged Brie":
+            return AgedBrieStrategy()
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            return BackstagePassStrategy()
+        elif item.name == "Sulfuras, Hand of Ragnaros":
+            return SulfurasStrategy()
+        else:
+            return NormalItemStrategy()
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            strategy = self._get_item_strategy(item)
+            strategy.update_quality(item)
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+
+    def test_aged_brie_quality(self):  # quality is > 50
+        items = [Item("Aged Brie", 0, 50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals(50, items[0].quality)
 
+    def test_apple_quality(self):  # quality
+        items = [Item("apple", 1, 12)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(11, items[0].quality)
+
+    def test_sulfuras_quality(self):  # Sulfuras never has to be sold or decreases in Quality
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 15)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(15, items[0].quality)  # should be 15
 
 if __name__ == '__main__':
     unittest.main()
