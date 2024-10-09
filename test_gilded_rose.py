d5c3e848fb39bf27d26ee14c041cb86fa1c1e665
iriszhang <zhang.tianyi13@northeastern.edu>
2024-10-08 21:23:13 -0700

    All tests pass

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -7,13 +7,13 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
 
-    def test_aged_brie_quality(self):  # quality is > 50
+    def test_aged_brie_quality(self):  # edge case - aged_brie only inc in quality, but quality cannot exceed 50
         items = [Item("Aged Brie", 0, 50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(50, items[0].quality)
 
-    def test_apple_quality(self):  # quality
+    def test_apple_quality(self):  # should dec 1 in quality
         items = [Item("apple", 1, 12)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
@@ -23,7 +23,7 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("Sulfuras, Hand of Ragnaros", 0, 15)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(15, items[0].quality)  # should be 15
+        self.assertEquals(15, items[0].quality)  # should remain 15
 
 if __name__ == '__main__':
     unittest.main()
